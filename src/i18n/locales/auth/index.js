c167b61ee7e4e0bedfc67ea8c0fa7d7d054f6018
xianluwan <xianluwan1@gmail.com>
2026-07-15 19:55:20 +0800

    Add files via upload

--- src/i18n/locales/auth/index.js
+++ src/i18n/locales/auth/index.js
@@ -2,19 +2,12 @@
 
 import zhCN from './zh-CN';
 
-import viVN from './vi-VN';
 
-import enUS from './en-US';
 
-import zhTW from './zh-TW';
 
-import jaJP from './ja-JP';
 
-import koKR from './ko-KR';
 
-import ruRU from './ru-RU';
 
-import faIR from './fa-IR';
 
 
 
@@ -22,18 +15,12 @@ export default {
 
   'zh-CN': zhCN,
 
-  'vi-VN': viVN,
 
-  'en-US': enUS,
 
-  'zh-TW': zhTW,
 
-  'ja-JP': jaJP,
 
-  'ko-KR': koKR,
 
-  'ru-RU': ruRU,
 
-  'fa-IR': faIR
+  // 仅保留简体中文
 
 };

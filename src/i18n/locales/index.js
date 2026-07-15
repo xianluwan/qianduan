7c47d1f4ccc0ae4703097f017fb930ddffa99cc7
xianluwan <xianluwan1@gmail.com>
2026-07-15 19:56:22 +0800

    Add files via upload

--- src/i18n/locales/index.js
+++ src/i18n/locales/index.js
@@ -1,18 +1,7 @@
 ﻿
 import zhCN from './zh-CN';
-import enUS from './en-US';
-import zhTW from './zh-TW';
-import jaJP from './ja-JP';
-import koKR from './ko-KR';
-import ruRU from './ru-RU';
-import faIR from './fa-IR';
 
 export default {
   'zh-CN': zhCN,
-  'en-US': enUS,
-  'zh-TW': zhTW,
-  'ja-JP': jaJP,
-  'ko-KR': koKR,
-  'ru-RU': ruRU,
-  'fa-IR': faIR
+  // 仅保留简体中文
 }; 
